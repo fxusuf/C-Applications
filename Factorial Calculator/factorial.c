23a02849a4cfee81649118a7794e936c8fe92f62
Yusuf <81730857+fxusuf@users.noreply.github.com>
2024-11-23 13:10:17 +0300

    Update factorial.c

--- Factorial Calculator/factorial.c
+++ Factorial Calculator/factorial.c
@@ -2,6 +2,7 @@
 Kullanıcıdan bir sayı alıp ve o sayının faktöriyelini hesapla
 negatif sayı girdiğinde hata verip tekrardan sayı girmesini sağla
 faktöriyelin nasıl hesaplandığını göstererek sonucu yazdır.
+Kullanıcıya devam etmek isteyip istemediğini sor.
 */
 #include <stdio.h>
 
